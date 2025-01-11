a9fcb7872c4db55aff52b0c989c6feabc6165a1b
Prathamesh Amrutkar <prathameshamrutkar@ternaengg.ac.in>
2025-01-12 01:42:20 +0530

    Changes

---
+++ src/com/example/Sample.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Prathamesh");
+		int a = 5, b = 6;
+		System.out.println("Sum is : "+a+b);
+	}
+
+}
